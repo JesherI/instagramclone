36bd733f54af7f5c9bff0fc97b2046c1586c3f8d
Jesher <jesherjesher1@gmail.com>
2023-12-10 16:38:44 -0600

    Home finish

--- app/src/main/java/com/neatroots/instagramclone/Post/PostActivity.kt
+++ app/src/main/java/com/neatroots/instagramclone/Post/PostActivity.kt
@@ -1,26 +1,33 @@
 package com.neatroots.instagramclone.Post
 
+
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.activity.result.contract.ActivityResultContracts
+
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
-import com.google.firebase.firestore.firestore
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import com.neatroots.instagramclone.HomeActivity
 import com.neatroots.instagramclone.Models.Post
+import com.neatroots.instagramclone.Models.User
+
 import com.neatroots.instagramclone.databinding.ActivityPostBinding
 import com.neatroots.instagramclone.utils.POST
 import com.neatroots.instagramclone.utils.POST_FOLDER
+import com.neatroots.instagramclone.utils.USER_NODE
+import com.neatroots.instagramclone.utils.USER_PROFILE_FOLDER
 import com.neatroots.instagramclone.utils.uploadImage
 
 
 class PostActivity : AppCompatActivity() {
-    val binding by lazy {
+    private val binding by lazy {
         ActivityPostBinding.inflate(layoutInflater)
     }
     var imageUrl: String? = null
-
     private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         uri?.let {
             uploadImage(uri, POST_FOLDER) { url ->
@@ -28,6 +35,7 @@ class PostActivity : AppCompatActivity() {
                     binding.selectImage.setImageURI(uri)
                     imageUrl = url
                 }
+
             }
         }
     }
@@ -35,33 +43,45 @@ class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         setSupportActionBar(binding.materialToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         binding.materialToolbar.setNavigationOnClickListener {
-            startActivity(Intent(this@PostActivity,HomeActivity::class.java))
+            startActivity(Intent(this@PostActivity, HomeActivity::class.java))
             finish()
         }
+
 
         binding.selectImage.setOnClickListener {
             launcher.launch("image/*")
         }
 
         binding.cancelButton.setOnClickListener {
-            startActivity(Intent(this@PostActivity,HomeActivity::class.java))
+            startActivity(Intent(this@PostActivity, HomeActivity::class.java))
             finish()
         }
 
         binding.postButton.setOnClickListener {
-            val post: Post = Post(imageUrl!!, binding.caption.editableText.toString())
+
+            val post: Post = Post(
+                postUrl = imageUrl!!,
+                caption = binding.caption.editableText.toString(),
+                uid = Firebase.auth.currentUser!!.uid,
+                time = System.currentTimeMillis().toString()
+            )
 
             Firebase.firestore.collection(POST).document().set(post).addOnSuccessListener {
-                Firebase.firestore.collection(Firebase.auth.currentUser!!.uid).document().set(post)
+                Firebase.firestore.collection(Firebase.auth.currentUser!!.uid).document()
+                    .set(post)
                     .addOnSuccessListener {
-                        startActivity(Intent(this@PostActivity,HomeActivity::class.java))
+                        startActivity(Intent(this@PostActivity, HomeActivity::class.java))
                         finish()
                     }
+
             }
+
+
         }
     }
 }
--- app/src/main/java/com/neatroots/instagramclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/neatroots/instagramclone/fragments/HomeFragment.kt
@@ -8,12 +8,21 @@ import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+import com.neatroots.instagramclone.Models.Post
 import com.neatroots.instagramclone.R
+import com.neatroots.instagramclone.adapers.PostAdapter
 import com.neatroots.instagramclone.databinding.FragmentHomeBinding
+import com.neatroots.instagramclone.utils.POST
 
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private var postList = ArrayList<Post>()
+    private lateinit var adapter: PostAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,8 +34,27 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        adapter = PostAdapter(requireContext(),postList)
+        binding.postRv.layoutManager = LinearLayoutManager(requireContext())
+        binding.postRv.adapter = adapter
+
         setHasOptionsMenu(true)
         (requireActivity() as AppCompatActivity).setSupportActionBar(binding.materialToolbar2)
+
+        Firebase.firestore.collection(POST).get().addOnSuccessListener {
+            var tempList = ArrayList<Post>()
+            postList.clear()
+            for (i in it.documents){
+
+                var post:Post = i.toObject<Post>()!!
+                tempList.add(post)
+
+            }
+            postList.addAll(tempList)
+            adapter.notifyDataSetChanged()
+
+        }
+
         return binding.root
     }
 
